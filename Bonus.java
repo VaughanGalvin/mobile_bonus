e6eb07533c17e27005d0b76ff86bf891179b6f3b
Vaughan Galvin <vaughan.galvin@gmail.com>
2021-01-22 18:05:13 +0300

    simple bonus added

---
+++ Bonus.java
@@ -0,0 +1,9 @@
+public class Bonus {
+    public static void main(String[] args) {
+        int balance_amount = 1000;
+        int topup_amount = 1600;
+        int bonus_sum = topup_amount / 100;
+
+        System.out.println(bonus_sum);
+    }
+}
